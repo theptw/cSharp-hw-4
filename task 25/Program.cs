f999322196712f8525e0fdb2167d3e82d6d7f185
Nikita <nikita.bykov485@yandex.com>
2022-07-11 11:54:53 +0500

    task25

---
+++ task 25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1;
+
+for (int i = 2; i <= num2; i++)
+{
+    result = result * num1;
+}
+
+Console.WriteLine($"Результат возведения {num1} в степень {num2} = {result} ");
