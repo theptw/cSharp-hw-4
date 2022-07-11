6dea18429c685e2d5ba4466b4ad3aa3123668da5
Nikita <nikita.bykov485@yandex.com>
2022-07-11 11:55:54 +0500

    task 29

---
+++ task 29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Не понимаю в чём проблема
+
+
+int[] FillArrRandom(int size)
+{
+    int[] arr = new int[size];
+    
+    
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next();
+    }
+    return arr;
+}
+
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"[{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] result = FillArrRandom(8);
+
+PrintArr(result);
