09faaf7fb98b25ff9bdaf9e0e240a3102b1695ea
Nikita <nikita.bykov485@yandex.com>
2022-07-11 12:03:49 +0500

    task 25 act

--- task 25/Program.cs
+++ task 25/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Введите первое число");
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+Console.WriteLine("Введите первое число");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
