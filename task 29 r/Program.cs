c5a314942eaea9f0a51192ba3cf088fbe29f224a
Nikita <nikita.bykov485@yandex.com>
2022-07-11 11:56:22 +0500

    task 29 test

---
+++ task 29 r/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] FillArrRandom(int size)
+{
+    int[] arr = new int[size];
+    int index = 0;
+
+    Random rnd = new Random();
+    while (index < arr.Length)
+    {
+        arr[index] = rnd.Next();
+        index++;
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"[{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+
+int[] result = FillArrRandom(8);
+PrintArr(result);
